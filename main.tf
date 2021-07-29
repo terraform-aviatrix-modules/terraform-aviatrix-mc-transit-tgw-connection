96b8ac873a88a54649f40d54da98da16bcbe9cd0
Dennizz <root@ipaddr.nl>
2021-07-29 23:39:29 +0200

    small cleanup

--- main.tf
+++ main.tf
@@ -19,12 +19,8 @@ resource "aws_customer_gateway" "transit_ha_gw" {
   }
 }
 
-resource "random_password" "psk_tunnel1" {
-  length  = 64
-  special = false
-}
-
-resource "random_password" "psk_tunnel2" {
+resource "random_password" "psk" {
+  count   = 2
   length  = 64
   special = false
 }
@@ -36,8 +32,8 @@ resource "aws_vpn_connection" "transit_gw" {
 
   tunnel1_inside_cidr   = var.tunnel_cidrs[0]
   tunnel2_inside_cidr   = var.tunnel_cidrs[1]
-  tunnel1_preshared_key = random_password.psk_tunnel1.result
-  tunnel2_preshared_key = random_password.psk_tunnel2.result
+  tunnel1_preshared_key = random_password.psk[0].result
+  tunnel2_preshared_key = random_password.psk[1].result
 }
 
 resource "aws_vpn_connection" "transit_ha_gw" {
@@ -48,8 +44,8 @@ resource "aws_vpn_connection" "transit_ha_gw" {
 
   tunnel1_inside_cidr   = var.tunnel_cidrs[2]
   tunnel2_inside_cidr   = var.tunnel_cidrs[3]
-  tunnel1_preshared_key = random_password.psk_tunnel1.result
-  tunnel2_preshared_key = random_password.psk_tunnel2.result
+  tunnel1_preshared_key = random_password.psk[0].result
+  tunnel2_preshared_key = random_password.psk[1].result
 }
 
 resource "aviatrix_transit_external_device_conn" "tunnel1_to_tgw" {
@@ -62,7 +58,7 @@ resource "aviatrix_transit_external_device_conn" "tunnel1_to_tgw" {
   remote_gateway_ip  = local.is_ha ? "${aws_vpn_connection.transit_gw.tunnel1_address},${aws_vpn_connection.transit_ha_gw[0].tunnel1_address}" : aws_vpn_connection.transit_gw.tunnel1_address
   local_tunnel_cidr  = local.is_ha ? "${cidrhost(var.tunnel_cidrs[0], 2)}/30,${cidrhost(var.tunnel_cidrs[2], 2)}/30" : "${cidrhost(var.tunnel_cidrs[0], 2)}/30"
   remote_tunnel_cidr = local.is_ha ? "${cidrhost(var.tunnel_cidrs[0], 1)}/30,${cidrhost(var.tunnel_cidrs[2], 1)}/30" : "${cidrhost(var.tunnel_cidrs[0], 1)}/30"
-  pre_shared_key     = random_password.psk_tunnel1.result
+  pre_shared_key     = random_password.psk[0].result
 }
 
 resource "aviatrix_transit_external_device_conn" "tunnel2_to_tgw" {
@@ -75,5 +71,5 @@ resource "aviatrix_transit_external_device_conn" "tunnel2_to_tgw" {
   remote_gateway_ip  = local.is_ha ? "${aws_vpn_connection.transit_gw.tunnel2_address},${aws_vpn_connection.transit_ha_gw[0].tunnel2_address}" : aws_vpn_connection.transit_gw.tunnel2_address
   local_tunnel_cidr  = local.is_ha ? "${cidrhost(var.tunnel_cidrs[1], 2)}/30,${cidrhost(var.tunnel_cidrs[3], 2)}/30" : "${cidrhost(var.tunnel_cidrs[1], 2)}/30"
   remote_tunnel_cidr = local.is_ha ? "${cidrhost(var.tunnel_cidrs[1], 1)}/30,${cidrhost(var.tunnel_cidrs[3], 1)}/30" : "${cidrhost(var.tunnel_cidrs[1], 1)}/30"
-  pre_shared_key     = random_password.psk_tunnel2.result
+  pre_shared_key     = random_password.psk[1].result
 }
